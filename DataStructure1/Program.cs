87471486b0e23a4f117a7fe968bbe01445e1e6e0
akinlami <bi54jf@studeent.sunderland.ac.uk>
2024-10-10 12:45:50 +0100

    Rather than fill the Main subroutine with lots of code, it is better to call methods/procedures to put your code into neat modules that can be called upon.
    Below I have completely re-written the Main subroutine and created some additional procedures: ListMovies(), ListShows(), and MainMenu().

--- DataStructure1/Program.cs
+++ DataStructure1/Program.cs
@@ -9,6 +9,12 @@ namespace DataStructure1
     internal class Program
     {
         static void Main(string[] args)
+
+        {
+            MainMenu();
+            Console.ReadKey();
+        }
+        private static void ListMovies()
         {
             Movie deadpool3;
             deadpool3.title = "Deadpool & Wolverine";
@@ -16,23 +22,73 @@ namespace DataStructure1
             deadpool3.price = 19.99;
             deadpool3.rating = MovieRating._15;
 
+            Console.Clear();
             Console.WriteLine(deadpool3.title + "(" + deadpool3.releaseYear + ")Rated " + deadpool3.rating);
+        }
 
-            Console.ReadKey();
-            Console.WriteLine("\n\n");
-            //Console.Clear();
+
+
+        private static void ListShows()
+        {
+
+
+
+
             List<TVShow> shows = new List<TVShow>();
             shows.Add(new TVShow("The Lord of the Rings: The Rings of Power", 2022, 0, MovieRating._15));
             shows.Add(new TVShow("The Boys", 2019, 7.99, MovieRating._18));
             shows.Add(new TVShow("Ben and Holly's Little Kingdom", 2009, 12.45, MovieRating._u));
 
+            Console.Clear();
 
             for (int i = 0; i < shows.Count; i++)
             {
                 Console.WriteLine(shows[i].ToString());
             }
-            Console.ReadKey();
+
+
         }
+
+
+        private static void MainMenu()
+        {
+
+            while (true)
+            {
+
+
+
+                Console.Clear();
+                Console.WriteLine("PleasE choose from the following options: \n");
+                Console.WriteLine("(1) List Movies");
+                Console.WriteLine("(2) List TVShows");
+                Console.WriteLine("\n");
+                Console.WriteLine("(0) Exit application");
+                Console.WriteLine("\n");
+
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int integerInput))
+                {
+                    switch (integerInput)
+
+                    {
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                        case 1:
+                            ListMovies();
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            ListShows();
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+            }
+        }
+
 
         public enum MovieRating
         {
